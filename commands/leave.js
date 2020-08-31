d3b29246323c3cdc4580a94b496a9d2777bf060b
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2020-08-31 17:44:47 +0200

    sound add sound and leave (bugged) commands

---
+++ commands/leave.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'leave',
+    description: 'this command makes Megumin leave the current voice channel..',
+    execute(message, args) {
+        //const channel = message.member.voice.channel;
+        connection.disconnect();
+    }
+}
