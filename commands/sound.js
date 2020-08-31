57d74c7605a037bff38fba4a7a5efe04852f695f
Paul Le Bras <paul.le-bras@epitech.eu>
2020-08-31 17:44:47 +0200

    sound add sound and leave (bugged) commands

---
+++ commands/sound.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'sound',
+    description: 'this is the soundboard command.',
+    execute(message, args) {
+        const channel = message.member.voice.channel;
+        const dispatcher = channel.join()
+            .then((connection) => {
+                connection.play(`soundboard/${args}.mp3`)
+                console.log(`soundboard/${args}.mp3`);
+            }).catch((error) => {
+                console.log(error);
+                message.channel.send(err);
+            });
+    }
+}
