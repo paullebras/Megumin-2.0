04225c7c7bcb3d9ec1f336ae3a85e03b2cea91e2
paul le bras <paul0.lebras@gmail.com>
2020-09-28 01:10:46 +0200

    logs authors

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'this is a ping command.',
     execute(message, args) {
-        message.channel.send('le Destin nous a réunis !');
+        message.channel.send(`${message.author.username}, le Destin nous a réunis !`);
     }
 }
