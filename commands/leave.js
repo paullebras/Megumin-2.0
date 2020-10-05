b47012ded36857a5e03353408f6bd686d3eec002
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2020-10-06 01:28:06 +0200

    add leave

--- commands/leave.js
+++ commands/leave.js
@@ -2,8 +2,7 @@ module.exports = {
     name: 'leave',
     description: 'this command makes Megumin leave the current voice channel..',
     execute(message, args) {
-        //const channel = message.member.voice.channel;
-        //connection.disconnect();
-        //message.channel.createMessage(`Thanks for tuning in!`); }
+        const voiceChannel = message.member.voice.channel;
+        voiceChannel.leave();
     }
 }
