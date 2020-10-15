d433cbbea1bc9a799b558f07cb00f5c87e9c1584
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2020-10-15 21:27:53 +0200

    nettoyage branche release

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ping',
     description: 'this is a ping command.',
     execute(message, args) {
+        //console.log('message.guild =', message.guild);
         message.channel.send(`${message.author.username}, le Destin nous a réunis !`);
     }
 }
