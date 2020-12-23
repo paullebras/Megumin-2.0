aad7adbcc0d9f223f484dc2f09ff29877bf11581
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2020-12-23 22:49:54 +0100

    add auto skip, stop and improve skip resume pause

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,3 @@
-const ytdl = require('ytdl-core');
 const voiceUtils = require('../utils/voiceUtils.js');
 const Config = require('../config/config.json');
 
@@ -11,49 +10,17 @@ module.exports = {
             const url = args[0];
             const channelToJoin = message.member.voice.channel;
             const currentChannel = client.voice.connections.get(Config.server_id);
-            // console.log('client.voice.connections =', client.voice.connections);
 
             await voiceUtils.joinVoice(channelToJoin, currentChannel, VoiceControl).catch((err) => {
                 throw err;
             })
             VoiceControl.queue.push(url);
             if (VoiceControl.isPlaying == false) {
-                await client.commands.get('play').play(VoiceControl);
-                VoiceControl.queueIndex += 1;
+                voiceUtils.playYoutube(VoiceControl);
             }
         } catch (err) {
             console.log(err);
             message.channel.send(err);
         }
     },
-
-
-    play(VoiceControl) {
-        return new Promise((resolve, reject) => {
-            try {
-                console.log('ca');
-                console.log('cb');
-                console.log('VoiceControl.queueIndex =', VoiceControl.queueIndex);
-                console.log('VoiceControl.queue[VoiceControl.queueIndex] =', VoiceControl.queue[VoiceControl.queueIndex]);
-                VoiceControl.dispatcher = VoiceControl.connection.play(ytdl(VoiceControl.queue[VoiceControl.queueIndex], { filter: 'audioonly' }))
-                    .on('start', () => {
-                        console.log('cc');
-                        // console.log('VoiceControl =', VoiceControl);
-                        console.log('cc2');
-                        VoiceControl.isPlaying = true;
-                    })
-                    .on('finish', () => {
-                        VoiceControl.isPlaying = false;
-                    })
-                    .on('error', (err) => {
-                        console.log(String(err));
-                        message.channel.send(String(err));
-                        reject();
-                    })
-                resolve();
-            } catch (err) {
-                reject(err);
-            }
-        })
-    }
 }
