0b9ea5ccc4f28e77a88bbbb8cf7997d62090b888
Paul Le Bras <paul.le-bras@epitech.eu>
2022-12-04 03:05:55 +0100

    add message embeding, reactMessage and sendMessage functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,33 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
 
-    logError: function (error, channel) {
-        const stringObj = new String(error);
+    logError: function(error, channel) {
+        const errorEmbed = new EmbedBuilder()
+            .setColor(0x0000)
+            .setDescription(`:x:\xa0\xa0${error}`);
 
         console.error(error);
-        channel.send(stringObj.toString());
+        channel.send({ embeds: [errorEmbed] });
         return;
     },
-}
+
+
+    sendMessage: function(text, channel) {
+        const messageEmbed = new EmbedBuilder()
+            .setColor(0x0000)
+            .setDescription(`:information_source:\xa0\xa0${text}`);
+
+        channel.send({ embeds: [messageEmbed] });
+        return;
+    },
+
+
+    reactMessage: function(reaction, message) {
+        message.react(reaction)
+            .catch((error) => {
+                this.logError(error, message.channel);
+            });
+        return;
+    },
+};
