ca24d65ceaad0ff21940d4b766879f1716d9ef43
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2022-12-04 03:12:23 +0100

    fix various bugs, add async/await, clean according to linter rules, add reaction to user msg, partial refactor of voice functions

--- src/commands/sound.js
+++ src/commands/sound.js
@@ -1,6 +1,6 @@
 const voiceUtils = require('../utils/voiceUtils.js');
 const utils = require('../utils/utils.js');
-const fs = require("fs");
+const fs = require('fs');
 const Path = require('path');
 
 module.exports = {
@@ -9,23 +9,32 @@ module.exports = {
     async execute(message, args, type, VoiceControl) {
         try {
             const channelToJoin = message.member.voice.channel;
-            const currentChannel = voiceUtils.getCurrentChannelFromMsg(VoiceControl, message);
+            const currentChannel = voiceUtils.getUserCurrentChannelFromMsg(message);
             const folder = type === 'sound' ? Path.join('src', 'soundboard') : Path.join('src', 'music');
-            const files = fs.readdirSync(folder)
+            const files = fs.readdirSync(folder);
             const normalizedFiles = files.map(x => x.toLowerCase());
             const index = normalizedFiles.indexOf(`${args[0].toLowerCase()}.mp3`);
-            const resource = files[index]
+            const resource = files[index];
 
             if (normalizedFiles.includes(args[0].toLowerCase())) {
-                throw (`\`\`\`File '${args}' does not exist\`\`\``);
+                throw (`File '${args}' does not exist`);
             }
             const path = Path.join(folder, resource);
-            await voiceUtils.joinVoice(channelToJoin, currentChannel, VoiceControl).catch((error) => {
-                throw (error);
-            });
-            voiceUtils.playResource(path, VoiceControl, { volume: 1.0 });
-        } catch (error) {
+
+            await voiceUtils.joinVoice(channelToJoin, currentChannel, VoiceControl)
+                .catch((error) => {
+                    throw (error);
+                });
+            await voiceUtils.playAudio(path, VoiceControl, { volume: 1.0 }, true)
+                .then(() => {
+                    utils.reactMessage('✅', message);
+                })
+                .catch((error) => {
+                    throw (error);
+                });
+        }
+        catch (error) {
             utils.logError(error, message.channel);
         }
-    }
-}
+    },
+};
--- src/utils/voiceUtils.js
+++ src/utils/voiceUtils.js
@@ -1,131 +1,190 @@
 const ytdl = require('ytdl-core');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, NoSubscriberBehavior } = require('@discordjs/voice');
-const utils = require('./utils.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
+const { NoSubscriberBehavior, AudioPlayerStatus } = require('@discordjs/voice');
+const utils = require('../utils/utils.js');
+const { createReadStream } = require('fs');
 
 module.exports = {
 
-    getCurrentChannelFromMsg: function (VoiceControl, message) {
-
-        let currentChannel;
+    getUserCurrentChannelFromMsg: function(message) {
+        return new Promise((resolve) => {
+            let currentChannel;
+            const connection = getVoiceConnection(process.env.SERVER_ID);
+            if (connection) {
+                const id = connection.joinConfig.channelId;
+                currentChannel = message.guild.channels.cache.find(element => (element.id.includes(id)));
+            }
+            resolve(currentChannel);
+        });
+        /* let currentChannel;
         if (VoiceControl.connection) {
             const id = VoiceControl.connection.joinConfig.channelId
             currentChannel = message.guild.channels.cache.find(element => (element.id.includes(id)));
         }
-        return currentChannel
+        return currentChannel */
     },
 
 
-    joinVoice: function (requestedChannel, currentChannel, VoiceControl) {
+    joinVoice: function(requestedChannel, currentChannel) {
         return new Promise((resolve, reject) => {
             try {
                 if (!requestedChannel && !currentChannel) {
-                    throw ("Désolée, l'un de nous deux doit être dans un canal vocal");
+                    throw ('Désolée, l\'un de nous deux doit être dans un canal vocal');
                 }
                 if (requestedChannel) {
                     if (currentChannel != requestedChannel) {
-                        VoiceControl.connection = joinVoiceChannel({
+                        joinVoiceChannel({
                             channelId: requestedChannel.id,
                             guildId: requestedChannel.guild.id,
                             adapterCreator: requestedChannel.guild.voiceAdapterCreator,
                         });
-                    } else {
-                        resolve();
                     }
                 }
                 resolve();
-            } catch (error) {
-                reject(error)
             }
-        })
+            catch (error) {
+                reject(error);
+            }
+        });
     },
 
 
-    destroyConnection: function (VoiceControl, connection) {
-        connection.destroy();
-        VoiceControl.connection = null;
-        return;
+    destroyConnection: function(VoiceControl) {
+        return new Promise((resolve, reject) => {
+            const connection = getVoiceConnection(process.env.SERVER_ID);
+            if (connection) {
+                connection.destroy();
+                VoiceControl.isPlaying = false;
+                VoiceControl.player = null;
+                resolve(VoiceControl);
+            }
+            else {
+                reject('Je ne suis actuellement connectée à aucun salon vocal.');
+            }
+        });
     },
 
 
-    pausePlayer: function (player) {
-        if (player !== null) {
-            player.pause();
-        } else {
-            throw ('\`\`\`# Le lecteur ne joue actuellement aucune musique.\`\`\`');
-        }
+    pausePlayer: function(player) {
+        return new Promise((resolve, reject) => {
+            if (player !== null) {
+                player.pause();
+                resolve();
+            }
+            else {
+                reject('Je ne joue actuellement aucune musique.');
+            }
+        });
     },
 
 
-    resumePlayer: function (player) {
-        if (player !== null) {
-            player.unpause();
-        } else {
-            throw ('\`\`\`# Le lecteur ne joue actuellement aucune musique.\`\`\`');
-        }
+    resumePlayer: function(player) {
+        return new Promise((resolve, reject) => {
+            if (player !== null) {
+                player.unpause();
+                resolve();
+            }
+            else {
+                reject('Je ne joue actuellement aucune musique.');
+            }
+        });
     },
 
 
-    stopPlayer: function (player) {
-        if (player !== null) {
-            player.stop();
-        } else {
-            throw ('\`\`\`# Le lecteur ne joue actuellement aucune musique.\`\`\`');
-        }
+    stopPlayer: function(player) {
+        return new Promise((resolve, reject) => {
+            if (player !== null) {
+                player.stop();
+            }
+            else {
+                reject('Je ne joue actuellement aucune musique.');
+            }
+        });
     },
 
 
-    playResource: function (path, VoiceControl, volume) {
-        const resource = createAudioResource(path);
+    playAudio: function(path, VoiceControl, volume/* , message */, playAsStream) {
+        return new Promise((resolve) => {
+            // start_issue
+            // 03/12/2022
+            // Using createReadStream should not be needed but there is an issue with discordjs/voice
+            // More details here: https://github.com/discordjs/discord.js/issues/7232
+            // original code: const resource = createAudioResource(path);
+            let resource;
+            if (playAsStream) {
+                resource = createAudioResource(createReadStream(path));
+            }
+            else {
+                resource = createAudioResource(path);
+            }
+            // end_issue
+
+            // const currentChannel = this.getUserCurrentChannelFromMsg(VoiceControl, message);
+            const connection = getVoiceConnection(process.env.SERVER_ID);
+            // const connection = getVoiceConnection(currentChannel.guild.id);
+
+            VoiceControl.player = createAudioPlayer({
+                behaviors: {
+                    // Default behavior is to pause when there are no active subscribers for an audio player.
+                    // Can be configured to pause, stop, or just continue playing through the stream.
+                    noSubscriber: NoSubscriberBehavior.Pause,
+                },
+            });
+            /* VoiceControl.player.on('stateChange', (oldState, newState) => {
+                console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+            }); */
+            VoiceControl.player.on('error', error => {
+                // console.error(`Error: ${error.message} with resource ${error.resource/* .metadata.title */}`);
+                throw (error);
+            });
+            VoiceControl.player.play(resource, volume);
+            VoiceControl.subscription = connection.subscribe(VoiceControl.player);
+            VoiceControl.player.on(AudioPlayerStatus.Playing, () => {
+                VoiceControl.isPlaying = true;
+                resolve();
+            });
+
+        });
+    },
+
+    // useless function, can be deleted after queue and life cycle have been handled
+    playYoutube: function(VoiceControl, message) {
+        const resource = createAudioResource(ytdl(VoiceControl.queue[VoiceControl.queueIndex], { filter: 'audioonly' }));
+
         VoiceControl.player = createAudioPlayer({
             behaviors: {
                 noSubscriber: NoSubscriberBehavior.Pause,
             },
         });
-        VoiceControl.player.play(resource, volume);
+        VoiceControl.player.play(resource);
+        VoiceControl.player
+            .on('info', (info/* , format, error */) => {
+                const videoDetails = info.videoDetails;
+                message.channel.send(`this song was requested: \`${videoDetails.title}\``);
+                VoiceControl.frontQueue.push(videoDetails.title);
+                if (VoiceControl.isPlaying == false) {
+                    this.playYoutube(VoiceControl, message);
+                }
+                // let fileName = info.title.replace(/[^a-z0-9\-]/gi, '_');
+                // let container = format.container;
+                // let writeableSteam = fs.createWriteStream(`${fileName}.${container}`);
+                // readableStream.pipe(writeableSteam);
+            })
+            .on('start', () => {
+                VoiceControl.isPlaying = true;
+            })
+            .on('finish', () => {
+                if (VoiceControl.queue.length >= VoiceControl.queueIndex + 2) {
+                    VoiceControl.queueIndex += 1;
+                    module.exports.playYoutube(VoiceControl, message);
+                }
+                else {
+                    VoiceControl.isPlaying = false;
+                }
+            })
+            .on('error', (error) => {
+                utils.logError(error.message, message.channel);
+            });
         VoiceControl.connection.subscribe(VoiceControl.player);
     },
-
-
-    playYoutube: function (VoiceControl, message) {
-        try {
-            const resource = createAudioResource(ytdl(VoiceControl.queue[VoiceControl.queueIndex], { filter: 'audioonly' }));
-
-            VoiceControl.player = createAudioPlayer({
-                behaviors: {
-                    noSubscriber: NoSubscriberBehavior.Pause,
-                },
-            });
-            VoiceControl.player.play(resource)
-            VoiceControl.player
-                .on('info', (info, format, error) => {
-                    const videoDetails = info.videoDetails;
-                    message.channel.send(`this song was requested: \`${videoDetails.title}\``);
-                    VoiceControl.frontQueue.push(videoDetails.title);
-                    if (VoiceControl.isPlaying == false) {
-                        voiceUtils.playYoutube(VoiceControl, message);
-                    }
-                    // let fileName = info.title.replace(/[^a-z0-9\-]/gi, '_');
-                    // let container = format.container;
-                    // let writeableSteam = fs.createWriteStream(`${fileName}.${container}`);
-                    // readableStream.pipe(writeableSteam);
-                })
-                .on('start', () => {
-                    VoiceControl.isPlaying = true;
-                })
-                .on('finish', () => {
-                    if (VoiceControl.queue.length >= VoiceControl.queueIndex + 2) {
-                        VoiceControl.queueIndex += 1;
-                        module.exports.playYoutube(VoiceControl, message);
-                    } else {
-                        VoiceControl.isPlaying = false;
-                    }
-                })
-                .on('error', (error) => {
-                    utils.logError(error.message, message.channel);
-                })
-            VoiceControl.connection.subscribe(VoiceControl.player);
-        } catch (error) {
-            throw (error);
-        }
-    }
-}
+};
