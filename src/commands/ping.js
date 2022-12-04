b72db876a90436ce80cf47383d18bf2509394245
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2022-12-04 02:43:08 +0100

    add embeded messages use

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,11 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+const utils = require('../utils/utils.js');
+
 module.exports = {
     name: 'ping',
     description: 'this is a ping command.',
     execute(message) {
-        message.channel.send(`${message.author/* .username */}, le Destin nous a réunis !`);
-        // To mention a specific role:
-        /* const roleName = 'Connard de Grammar Nazi'
-        const roleId = '466892791243603968'
-        message.channel.send(`${message.guild.roles.cache.find(element => (element.name.includes(roleName)) || element.id.includes(roleId))}`); */
-    }
-}
+        try {
+            message.channel.send(`${message.author/* .username */}, le Destin nous a réunis !`);
+
+            const exampleEmbed = new EmbedBuilder()
+                .setColor(0x0000)
+                .setImage('https://i.ytimg.com/vi/2MeXQd41H_Q/maxresdefault.jpg');
+            message.channel.send({ embeds: [exampleEmbed] });
+            // To mention a specific role:
+            // const roleName = 'Connard de Grammar Nazi'
+            // const roleId = '466892791243603968'
+            // message.channel.send(`${message.guild.roles.cache.find(element => (element.name.includes(roleName)) || element.id.includes(roleId))}`);
+        }
+        catch (error) {
+            utils.logError(error, message.channel);
+        }
+    },
+};
