b1917b311b4bc5caf32cefd6f9fd21389d8a846a
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2022-12-11 18:16:12 +0100

    improve file structure via conditional inversion

--- src/commands/play.js
+++ src/commands/play.js
@@ -5,11 +5,19 @@ const utils = require('../utils/utils.js');
 module.exports = {
     name: 'play',
     description: 'Joue l\'audio d\'une vidéo sur Youtube. A l\'avenir, divers sites seront pris en charge.',
-    usage:'play <youtube URL>',
+    usage: 'play <youtube URL>',
     type: ':notes: Music',
     async execute(message, args, VoiceControl) {
         try {
             const url = args[0];
+
+            if (!url.includes('http://') && !url.includes('https://')) {
+                throw ('La recherche de vidéos par titre n\'a pas encore été implémentée');
+            }
+            if (!url.includes('giyoutube') && !url.includes('youtu.be')) {
+                throw ('La lecture de contenus extérieurs à YouTube n\'a pas encore été implémentée');
+            }
+
             const channelToJoin = message.member.voice.channel;
             const currentChannel = voiceUtils.getUserCurrentChannelFromMsg(message);
 
@@ -24,39 +32,27 @@ module.exports = {
                 bitrate: 128,
                 quality: 'lowestaudio',
             };
-
             await voiceUtils.joinVoice(channelToJoin, currentChannel, VoiceControl)
                 .catch((error) => {
                     throw (error);
                 });
-            if (url.includes('http://') || url.includes('https://')) {
-                VoiceControl.queue.push(url);
-                if (url.includes('youtube') || url.includes('youtu.be')) {
-                    const readableStream = ytdl(url);
-                    readableStream
-                        .on('info', (info/* , format */) => {
-                            VoiceControl.frontQueue.push(info.videoDetails.title);
-                            // let fileName = info.title.replace(/[^a-z0-9\-]/gi, '_');
-                            // let container = format.container;
-                            // let writeableSteam = fs.createWriteStream(`${fileName}.${container}`);
-                            // readableStream.pipe(writeableSteam);
-                        })
-                        .on('error', (error/* , format */) => {
-                            utils.logError(error.message);
-                        });
-                    if (VoiceControl.isPlaying == false) {
-                        const audio = ytdl(VoiceControl.queue[VoiceControl.queueIndex], audioParams);
-                        voiceUtils.playAudio(audio, VoiceControl, { volume: 1.0 }/* , message */, false);
-                    }
-                }
-                else {
-                    throw ('La lecture de contenus extérieurs à youtube n\'a pas encore été implémentée');
-                }
+            VoiceControl.queue.push(url);
+            const readableStream = ytdl(url);
+            readableStream
+                .on('info', (info/* , format */) => {
+                    VoiceControl.frontQueue.push(info.videoDetails.title);
+                    // let fileName = info.title.replace(/[^a-z0-9\-]/gi, '_');
+                    // let container = format.container;
+                    // let writeableSteam = fs.createWriteStream(`${fileName}.${container}`);
+                    // readableStream.pipe(writeableSteam);
+                })
+                .on('error', (error/* , format */) => {
+                    utils.logError(error.message);
+                });
+            if (VoiceControl.isPlaying == false) {
+                const audio = ytdl(VoiceControl.queue[VoiceControl.queueIndex], audioParams);
+                voiceUtils.playAudio(audio, VoiceControl, { volume: 1.0 }/* , message */, false);
             }
-            else {
-                throw ('La recherche de vidéos par titre n\'a pas encore été implémentée');
-            }
-
         }
         catch (error) {
             utils.logError(error, message.channel);
