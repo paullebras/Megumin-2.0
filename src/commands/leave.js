adda74293ab82a370cba5d485092b2fee4e3ed62
Paul Le Bras <36215166+paullebras@users.noreply.github.com>
2024-04-01 23:04:30 +0200

    chore(config): add and use prettier formatter

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -6,22 +6,19 @@ const name = 'leave';
 const description = 'Quitte le salon vocal actuel.';
 
 module.exports = {
-    name: name,
-    description: description,
-    usage: 'leave',
-    type: ':notes: Music',
-    data: new SlashCommandBuilder()
-        .setName(name)
-        .setDescription(description),
-    async execute(interaction, VoiceControl) {
-        const connection = getVoiceConnection(process.env.SERVER_ID);
-        if (!connection) {
-            throw new Error('Désolée, je ne suis pas dans un canal vocal.');
-        }
-        await voiceUtils.destroyConnection(VoiceControl)
-            .catch((error) => {
-                throw (error);
-            });
-        return { content: '`Left voice channel` ✅' };
-    },
+  name: name,
+  description: description,
+  usage: 'leave',
+  type: ':notes: Music',
+  data: new SlashCommandBuilder().setName(name).setDescription(description),
+  async execute(interaction, VoiceControl) {
+    const connection = getVoiceConnection(process.env.SERVER_ID);
+    if (!connection) {
+      throw new Error('Désolée, je ne suis pas dans un canal vocal.');
+    }
+    await voiceUtils.destroyConnection(VoiceControl).catch((error) => {
+      throw error;
+    });
+    return { content: '`Left voice channel` ✅' };
+  },
 };
--- src/commands/sound.js
+++ src/commands/sound.js
@@ -5,57 +5,61 @@ const { createAudioResource } = require('@discordjs/voice');
 const { SlashCommandBuilder } = require('discord.js');
 
 const name = 'sound';
-const description = 'Joue un audio spécifique ou bien en choisis un aléatoirement';
-const inputDescription = '<nom de l\'audio> ou \'random\' pour jouer un son aléatoire';
+const description =
+  'Joue un audio spécifique ou bien en choisis un aléatoirement';
+const inputDescription =
+  "<nom de l'audio> ou 'random' pour jouer un son aléatoire";
 
 module.exports = {
-    name: name,
-    description: description,
-    usage: `${name} ${inputDescription}`,
-    type: ':notes: Music',
-    data: new SlashCommandBuilder()
-        .setName(name)
-        .setDescription(description)
-        .addStringOption(option =>
-            option.setName('input')
-                .setDescription(inputDescription)),
-    async execute(interaction, args, type, VoiceControl) {
-        const channelToJoin = interaction.member.voice.channel;
-        const currentChannel = await interaction.voiceChannel;
-        const folder = type === 'sound' ? Path.join('src', 'soundboard') : Path.join('src', 'music');
-        const files = readdirSync(folder);
-        const normalizedFiles = files.map(x => x.toLowerCase());
+  name: name,
+  description: description,
+  usage: `${name} ${inputDescription}`,
+  type: ':notes: Music',
+  data: new SlashCommandBuilder()
+    .setName(name)
+    .setDescription(description)
+    .addStringOption((option) =>
+      option.setName('input').setDescription(inputDescription),
+    ),
+  async execute(interaction, args, type, VoiceControl) {
+    const channelToJoin = interaction.member.voice.channel;
+    const currentChannel = await interaction.voiceChannel;
+    const folder =
+      type === 'sound'
+        ? Path.join('src', 'soundboard')
+        : Path.join('src', 'music');
+    const files = readdirSync(folder);
+    const normalizedFiles = files.map((x) => x.toLowerCase());
 
-        let index;
-        if (args[0] === 'random') {
-            index = Math.floor(Math.random() * normalizedFiles.length);
-        }
-        else {
-            index = normalizedFiles.indexOf(`${args[0].toLowerCase()}.mp3`);
-        }
-        if (index === -1) {
-            throw new Error(`File '${args}' does not exist`);
-        }
-        const resource = files[index];
-        const resourcePath = Path.join(folder, resource);
+    let index;
+    if (args[0] === 'random') {
+      index = Math.floor(Math.random() * normalizedFiles.length);
+    } else {
+      index = normalizedFiles.indexOf(`${args[0].toLowerCase()}.mp3`);
+    }
+    if (index === -1) {
+      throw new Error(`File '${args}' does not exist`);
+    }
+    const resource = files[index];
+    const resourcePath = Path.join(folder, resource);
 
-        await voiceUtils.joinVoice(channelToJoin, currentChannel)
-            .catch((error) => {
-                throw (error);
-            });
-        VoiceControl.source = 'soundboard';
-        // start_issue
-        // 03/12/2022
-        // Using createReadStream should not be needed but there is an issue with discordjs/voice
-        // More details here: https://github.com/discordjs/discord.js/issues/7232
-        // original code: const resource = createAudioResource(path);
-        const audioResource = createAudioResource(createReadStream(resourcePath));
-        // end_issue
-        await voiceUtils.playAudioResource(audioResource, VoiceControl)
-            .catch((error) => {
-                throw (error);
-            });
+    await voiceUtils.joinVoice(channelToJoin, currentChannel).catch((error) => {
+      throw error;
+    });
+    VoiceControl.source = 'soundboard';
+    // start_issue
+    // 03/12/2022
+    // Using createReadStream should not be needed but there is an issue with discordjs/voice
+    // More details here: https://github.com/discordjs/discord.js/issues/7232
+    // original code: const resource = createAudioResource(path);
+    const audioResource = createAudioResource(createReadStream(resourcePath));
+    // end_issue
+    await voiceUtils
+      .playAudioResource(audioResource, VoiceControl)
+      .catch((error) => {
+        throw error;
+      });
 
-        return { content: `\`Playing ${resource}\` ✅` };
-    },
+    return { content: `\`Playing ${resource}\` ✅` };
+  },
 };
