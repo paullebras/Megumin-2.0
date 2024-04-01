c60aa59a2d19053e414970de798ec4b0dd14e71e
sanguor <36215166+paullebras@users.noreply.github.com>
2024-04-01 23:04:30 +0200

    chore(config): add and use prettier formatter

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    apps: [
-        {
-            name: 'megumin', // Replace with your app's name
-            script: 'app.js', // Main entry file
-            instances: 1, // Number of instances you want to run
-            autorestart: true, // Restart the app on crashes
-            watch: false, // Set to true to watch for file changes and restart
-            max_memory_restart: '1G', // Maximum memory limit for restarting
-            env: {
-                NODE_ENV: 'production', // Environment mode
-            },
-        },
-    ],
+  apps: [
+    {
+      name: 'megumin', // Replace with your app's name
+      script: 'app.js', // Main entry file
+      instances: 1, // Number of instances you want to run
+      autorestart: true, // Restart the app on crashes
+      watch: false, // Set to true to watch for file changes and restart
+      max_memory_restart: '1G', // Maximum memory limit for restarting
+      env: {
+        NODE_ENV: 'production', // Environment mode
+      },
+    },
+  ],
 };
